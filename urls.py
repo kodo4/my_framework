4bfe578f7ad7d1859d3bff2b9a0b4da34f956066
Роман Кодочигов <kodochigovra@mail.ru>
2022-03-13 15:59:42 +0300

    add models

--- urls.py
+++ urls.py
@@ -1,6 +1,12 @@
-from views import Index, About
+from views import Index, About, StudyPrograms, CoursesList, \
+    CreateCourse, CreateCategory, CategoryList
 
 routes = {
     '/': Index(),
     '/about/': About(),
+    '/study-programs/': StudyPrograms(),
+    '/courses-list/': CoursesList(),
+    '/create-course/': CreateCourse(),
+    '/create-category/': CreateCategory(),
+    '/category-list/': CategoryList(),
 }
